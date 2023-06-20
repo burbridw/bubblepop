793dcd31f723dba7543facff4f423ed0afe4b723
burbridw <111381782+burbridw@users.noreply.github.com>
2023-06-21 06:48:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-const ctx = canvas1.getContext("2d")
-canvas1.width = 500
-canvas1.height = 500
+window.addEventListener("load",()=>{
+    const ctx = canvas1.getContext("2d")
+    canvas1.width = 500
+    canvas1.height = 500
+
+    class game {
+        constructor(width, height) {
+            this.width = width
+            this.height = height
+        }
+        update() {
+
+        }
+        draw() {
+
+        }
+    }
+})
+
+class bubble {
+    constructor(game){
+        this.game = game
+        this.width = 300
+        this.height = 300
+    }
+}
+
+
 
 function drawCanvas() {
     ctx.drawImage(bubble, 0, 0, 300, 300, 0, 0, 300, 300)
